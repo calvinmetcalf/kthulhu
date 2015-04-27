f498b89fa4a5c34bd90adf94a819855f38607a5a
Calvin Metcalf <cmetcalf@appgeo.com>
2015-04-27 15:43:34 -0400

    tests

--- test/test.js
+++ test/test.js
@@ -19,3 +19,21 @@ test("test", function (t) {
     t.end()
   })
 });
+
+test("multiple calls to next", function (t) {
+  var rs = fs.createReadStream(__dirname + "/test")
+    .pipe(go(function (chunk, _, next) {
+      setImmediate(next);
+      return chunk.toString().split("").reverse().join("").toUpperCase()
+    }))
+
+  var str = ""
+  rs.on("data", function (chunk) {
+    str += chunk.toString()
+  })
+
+  rs.on("finish", function () {
+    t.equal("\nTSET A SI SIHT", str)
+    t.end()
+  })
+});
